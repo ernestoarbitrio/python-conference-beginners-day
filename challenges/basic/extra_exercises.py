ef20e19cb596326a0c15705f702c5902f2ea4a50
Ernesto Arbitrio <ernesto.arbitrio@gmail.com>
2017-04-06 14:28:03 +0200

    pandas exercises

---
+++ challenges/basic/extra_exercises.py
@@ -0,0 +1,48 @@
+"""
+EXERCISES:
+========================================================================================================================
+Es.1
+- Question:
+Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
+between 2000 and 3200 (both included).
+The numbers obtained should be printed in a comma-separated sequence on a single line.
+
+Hints: 
+Consider use range(#begin, #end) method
+
+
+Es.2
+Question:
+With a given integral number n, write a program to generate a dictionary that contains (i, i*i) such that is an 
+integral number between 1 and n (both included). and then the program should print the dictionary.
+Suppose the following input is supplied to the program:
+8
+Then, the output should be:
+{1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
+
+Hints:
+In case of input data being supplied to the question, it should be assumed to be a console input.
+Consider use dict()
+
+
+Es.3
+Write a Python program to check whether a specified value is contained in a group of values.
+Test Data : 
+3 -> [1, 5, 8, 3] : True
+-1 -> [1, 5, 8, 3] : False
+
+
+Es.4
+Write a Python program to print out a set containing all the colors from color_list_1 which are not present in 
+color_list_2.
+Test Data : 
+color_list_1 = set(["White", "Black", "Red"]) 
+color_list_2 = set(["Red", "Green"])
+Expected Output : 
+{'Black', 'White'}
+
+Es.5
+Write a Python program to add two objects if both objects are an integer type.
+Hint: use isinstance built-in function to check the type of an objects
+========================================================================================================================
+"""
